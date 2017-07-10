0723bacf4c1725be075900e09269cbdbfae3f489
Gabriel Lins <gabriel.linss@hotmail.com>
2017-07-10 16:30:31 -0300

    Deu xabu, é?

--- app/src/main/java/com/example/admin/gipp_ol/MainActivity.java
+++ app/src/main/java/com/example/admin/gipp_ol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.gipp_ol;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this, BarCodeActivity.class);
+                startActivity(i);
+
             }
         });
 
@@ -93,7 +95,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-        return (false);
+        return (true);
 
     }
 
