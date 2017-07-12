53ddccf25ed39c77e2d8135a04386aee9862da7a
Erick Santos <erick.j.rocha@gmail.com>
2017-07-12 12:16:02 -0300

    Update LoginActivity.java
    Com todas as permissões para android acima de 5.1.1

--- app/src/main/java/com/example/admin/gipp_ol/LoginActivity.java
+++ app/src/main/java/com/example/admin/gipp_ol/LoginActivity.java
@@ -9,30 +9,25 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
@@ -87,6 +82,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mUserSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent logavaipromain = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(logavaipromain);
                 //attemptLogin();
